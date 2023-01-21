b46f1a106b8efc011149c4948c72af799c82472f
许心怡 <3458612409@qq.com>
2023-01-21 12:45:55 +0800

    修改静态资源路径

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,12 @@
 module.exports = {
+    title: 'lishiminlijiancheng-ui',
+    base: '/',
     themeConfig: {
+      nav: [
+        {text: 'Home', link: '/'},
+        {text: 'Github', link: 'https://github.com/lishiminlijiancheng/lishiminlijiancheng-ui.git'},
+        {text: 'Npm', link: 'https://www.npmjs.com/package/lishiminlijiancheng-ui'}
+      ],
       sidebar: [
         '/',
         '/componentsDocs/card'
